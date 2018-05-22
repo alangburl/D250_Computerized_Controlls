fca06bcb2c228ca42e487acfec73b2cf5bb643f8
Alan Burl <alangburl@gmail.com>
2018-05-21 22:36:14 -0500

    Defined Class for crusie control module

--- cruise control.py
+++ cruise control.py
@@ -1,21 +1,11 @@
 import numpy as np
-from sympy import *
 
 '''Defining set-up parameters'''
 cruise=None
 
-
-'''Logical statements'''
-def cruise_on_off(cruise):
-    while switch=='On' and cruise=='On':
-        switch=input("Switch Position: ")
-        brake_pedal=input("Brake Pedal- Released or Depressed: ")
-        set_button=input("Set Button- Released or Depressed: ")
-        
-        if brake_pedal=='Depressed':
-            cruise='Off'
-            
-        else:
-            cruise='On'
-        
-    return cruise 
+class cruise_control():
+	def __init__(self):
+		super().__init__(self)
+	
+	
+	
