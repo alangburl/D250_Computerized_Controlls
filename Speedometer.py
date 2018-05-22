763b9c4c571c1e11c4348fd19387eca17122fc6b
Alan Burl <alangburl@gmail.com>
2018-05-21 23:27:33 -0500

    Updates to Speedomter and Cruise Control to include classes for defining GUI and desired states

---
+++ Speedometer.py
@@ -0,0 +1,12 @@
+class speedometer():
+    def __init__(self):
+        super().__init__()
+        
+    def find_speed(self):
+        ''''Determine the speed from the GPIO input pins''' 
+        # =============================================================================
+        # define GPIO stuff here
+        # =============================================================================
+        speed_in_mph=10
+        return speed_in_mph
+    
