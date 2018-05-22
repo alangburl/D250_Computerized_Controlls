95635a49ca13385212f75e7c0d3a370426ce6478
Alan Burl <alangburl@gmail.com>
2018-05-22 18:22:03 -0500

    Make GUI work with speedometer manual input

--- cruise_control.py
+++ cruise_control.py
@@ -11,16 +11,17 @@ class Window(QWidget):
         self.set_speed = QLCDNumber(self)
         self.button = QPushButton('Start', self)
         self.progress = QProgressBar(self)
-        self.button.clicked.connect(self.initUI)
+#        self.button.clicked.connect(self.initUI)
         self.close=QPushButton('Close', self)
-        self.close.clicked.connect(self.closeEvent)        
+#        self.close.clicked.connect(self.closeEvent) 
+        self.cruise=cruise_control()
         layout = QVBoxLayout(self)
-        layout.addWidget(self.spinbox)
         layout.addWidget(self.close)
-        layout.addWidget(self.lcdnumber)
+        layout.addWidget(self.set_speed)
         layout.addWidget(self.button)
         layout.addWidget(self.progress)
         self.setWindowTitle('Gauges')
+        self.set_speed.display(self.cruise.set_speed())
         
         
 '''deterimining the deisred state'''
@@ -28,7 +29,6 @@ import Speedometer as sp
 
 class cruise_control():
     desired_speed=0
-    set_speed=None
     def __init__(self):
         super().__init__()
         self.set_speed()        
@@ -37,23 +37,28 @@ class cruise_control():
         
     def set_speed(self):
         '''Set the current read speed as the desired speed'''
-        a=sp.speedometer.find_speed(self)
-        print(a)
+        self.desired_speed=sp.speedometer.find_speed(self)
+#        print(a)
+        return self.desired_speed
     
     def run(self):
         '''Maintain the deisred speed via a PID class'''
+        self.actual_speed=sp.speedometer.find_speed(self)
         
+        while self.on_off==True:
+            do_something_for_pid_controller=None 
         
     def on_off(self):
         '''Determine whether the crusie module is active'''
+        on_off=True
+        return on_off
         
     def cancel(self):
         '''Cancels cruise, but still maintains the knowns speed'''
         
         
 if __name__=="__main__":
-    ex=cruise_control()
-	app = QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = Window()
     window.show()
     sys.exit(app.exec_())
