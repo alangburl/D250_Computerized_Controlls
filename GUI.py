5a5ac9c8cc0e4896b805a85c40ea6032b4351b74
Alan Burl <alangburl@gmail.com>
2018-05-31 19:29:06 -0500

    Add GUI file

--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 import sys, time
 from PyQt5.QtWidgets import (QWidget, QApplication, QPushButton, 
                              QLCDNumber, QVBoxLayout, QProgressBar,QCheckBox,
-                             QRadioButton,QHBoxLayout)
+                             QRadioButton,QHBoxLayout,QGridLayout,QLineEdit)
 from PyQt5.QtCore import QThread, pyqtSignal
 
 
@@ -10,22 +10,28 @@ class Display(QWidget):
         super().__init__()
         
         self.throttle_position=QProgressBar(self)
+        self.throttle_position.setMinimumHeight(50)
         self.speed=QLCDNumber(self)
+        self.speed.setMinimumHeight(50)
         self.set_speed=QLCDNumber(self)
-        self.cruise_indicator=QRadioButton(self)
+        self.set_speed.setMinimumHeight(50)
+        self.cruise_indicator=QLineEdit(self)
+        self.cruise_indicator.setMinimumHeight(50)
+        self.set_cruise=QPushButton('Set',self)
+        self.resume=QPushButton('Resume',self)
+        self.cancel=QPushButton('Cancel',self)
+        self.cruise_state=QPushButton('On\Off',self)
+        'Add the layout to the widget'
         
-        self.cruise_widget=QWidget(self)
-        
-        self.cruise_layout=QHBoxLayout(self)
-        self.cruise_layout.addWidget(self.cruise_indicator)
-        self.cruise_layout.addWidget(self.set_speed)
-        self.cruise_widget.layout=self.cruise_layout
-        
-        self.layout=QVBoxLayout(self)
-        self.layout.addWidget(self.throttle_position)
-        self.layout.addWidget(self.speed)
-        self.layout.addWidget(self.cruise_widget)
-        
+        self.layout=QGridLayout(self)
+        self.layout.addWidget(self.speed,0,1)
+        self.layout.addWidget(self.set_speed,2,1)
+        self.layout.addWidget(self.cruise_indicator,1,0)
+        self.layout.addWidget(self.resume,0,2)
+        self.layout.addWidget(self.set_cruise,1,2)
+        self.layout.addWidget(self.cancel,3,2)
+        self.layout.addWidget(self.cruise_state,4,2)
+        self.setLayout(self.layout)
     
         
         
